8219930e777e54fe6be01bfe2da1349f7a482ff7
leslientodi <lesliehazard500@gmail.com>
2024-10-10 12:35:23 +0000

    set Update Client By ID

--- server/index.js
+++ server/index.js
@@ -96,3 +96,25 @@ app.put("/client/:client", async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   }
 });
+
+app.put("/client/:clientID", async (req, res) => {
+  try {
+    const client = req.params.clientID;
+    const updateData = req.body;
+
+    // Find user by username and update with new data
+    const user = await UserModel.findOneAndUpdate(
+      { clientID }, // Query: find by username
+      { $set: updateData }, // Data to update
+      { new: true } // Return the updated document
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user); // Send the updated user data as response
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
